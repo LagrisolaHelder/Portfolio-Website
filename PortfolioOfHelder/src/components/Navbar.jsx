4d6e705a08d2cbe0e916d558b00bf77db5ac2023
Helder Lagrisola <helderlagrisola@gmail.com>
2026-07-22 13:51:50 +0100

    feat: enhance Navbar component with improved structure and styling

--- PortfolioOfHelder/src/components/Navbar.jsx
+++ PortfolioOfHelder/src/components/Navbar.jsx
@@ -2,26 +2,31 @@
 
 const Navbar = () => {
  return(
-
-    <div className="navbar bg-base-100 shadow-sm">
+    
+    <header className="bg-base-100  sticky top-0 z-50"> 
+             <div className="navbar bg-base-100 max-w-5xl mx-auto">
   <div className="flex-1">
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    <a className="btn btn-ghost text-xl">Helder Lagrisola</a>
   </div>
   <div className="flex-none">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Link</a></li>
+      <li><a className="bg-gray-900 text-white rounded-2xl mx-5 hover:bg-white hover:text-black hover:shadow-sm">About Me</a></li>
       <li>
         <details>
-          <summary>Parent</summary>
+  <summary className="list-none cursor-pointer [&::-webkit-details-marker]:hidden after:hidden marker:content-none">
+  <i className="fa-solid fa-bars"></i>
+</summary>
           <ul className="bg-base-100 rounded-t-none p-2">
-            <li><a>Link 1</a></li>
-            <li><a>Link 2</a></li>
+            <li><a>Projects</a></li>
+            <li><a>Contact</a></li>
           </ul>
         </details>
       </li>
     </ul>
   </div>
 </div>
+    </header>
+ 
  )
 }
 
